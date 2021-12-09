2d3e3c0b278db17401750597f9e481ab910b0f1f
Tray Chea <84699682+Zavvy-Glitch@users.noreply.github.com>
2021-12-08 17:23:49 -0800

    Delete virtualstore directory

--- virtualstore/src/components/footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer>
-      <h4>Still under Maintenance ....</h4>
-    </footer>
-  );
-}
--- virtualstore/src/components/header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Header() {
-  return (
-    <header>
-      <h1>Virtual Store</h1>
-    </header>
-  );
-}
