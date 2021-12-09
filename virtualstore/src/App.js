82d63cac9c4414972bdb790f3164654b29048c67
chattray <chea0073@gmail.com>
2021-12-08 17:06:54 -0800

    adds products / stylings

--- virtualstore/src/App.js
+++ virtualstore/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
+import Box from '@mui/material/Card';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
 
-function App() {
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Grid container spacing={8} 
+            justifyContent='center'
+            sx={{margin:'250px 0px 0px 0px'}}>
+              
+      {props.count.products.map((product, idx) => {
+        return (
+          <Grid item key={idx}>
+          <Box sx={{width: 350, 
+                    height:250, 
+                    background:'#0097a7', 
+                    outline:'solid'
+                  }}>
+            <h3>{product.name}</h3>
+
+            <p>Number Available: {product.count}</p>
+            <p>Price: {product.price}</p>
+            <p>Description: {product.description}</p>
+
+          <Button sx={{background:'#1de9b6', 
+                       color:'black'}}
+                  variant='outlined'
+                  onClick={() => {
+              props.decrement(product.count);
+              console.log(props.decrement(product.count))
+            }}> Place Item in Cart
+          </Button>
+          </Box>
+          </Grid>
+        );
+      })}
+      </Grid>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    count: state.count,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  decrement: (count) => dispatch({ type: 'DECREMENT', payload: count }),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
